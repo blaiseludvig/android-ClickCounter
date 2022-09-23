0298e761a0e8fd0e7f0812395a0a4e27b790c5f9
blaiseludvig <blaiseludvig@gmail.com>
2022-09-23 12:24:02 +0200

    add base functionality

---
+++ app/src/main/java/com/example/clickcounter/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.clickcounter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnPlus;
+    private TextView textView;
+    private Button btnMinus;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        init();
+
+    }
+
+    private void init() {
+        setComponents();
+        addEventListeners();
+
+
+
+    }
+
+    private void addEventListeners() {
+        btnPlus.setOnClickListener(view -> {
+            int n = Integer.parseInt(textView.getText().toString());
+            n += 1;
+            textView.setText(String.valueOf(n));
+
+            if (n > 0)  {
+                textView.setTextColor(Color.GREEN);
+            }
+
+            if (n == 0) {
+                textView.setTextColor(Color.parseColor("#00FFFF"));
+            }
+
+        });
+
+        btnMinus.setOnClickListener(view -> {
+            int n = Integer.parseInt(textView.getText().toString());
+            n -= 1;
+            textView.setText(String.valueOf(n));
+
+            if (n < 0){
+                textView.setTextColor(Color.RED);
+            }
+
+            if (n == 0) {
+                textView.setTextColor(Color.parseColor("#00ffFF"));
+            }
+
+        });
+    }
+
+    private void setComponents() {
+        btnPlus = findViewById(R.id.btnPlus);
+        textView = findViewById(R.id.textView);
+        btnMinus = findViewById(R.id.btnMinus);
+
+    }
+
+
+}
